d42ad52a11710289be008201a34e6bff15e272ea
Lucas Barbosa <lucas2304barbosa@gmail.com>
2025-04-15 08:43:32 -0300

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const produtos = [
+    {imagem:'https://www.americanas.com.br/_next/image?url=https%3A%2F%2Famericanas.vtexassets.com%2Farquivos%2Fids%2F462397%2F1638790540_1SZ.jpg%3Fv%3D638796563477300000&w=1280&q=75', produto: 'Ovo de Páscoa Sonho de Valsa com Recheio', preco: 59.90},
+    {imagem: 'https://www.americanas.com.br/_next/image?url=https%3A%2F%2Famericanas.vtexassets.com%2Farquivos%2Fids%2F577302%2F7514250775_1SZ.jpg%3Fv%3D638796546541430000&w=1280&q=75', produto: 'Ovo de Páscoa Chocolate ao Leite', preco: 69.90},
+    {imagem: 'https://www.americanas.com.br/_next/image?url=https%3A%2F%2Famericanas.vtexassets.com%2Farquivos%2Fids%2F408669%2F7087418610_1SZ.jpg%3Fv%3D638790693546970000&w=1280&q=75', produto: 'Ovo de Páscoa Patrulha Canina de Chocolate', preco: 79.90}
+];
+
+let conteudoProdutos = '';
+produtos.forEach( function(produto){
+    conteudoProdutos += '<div class="caixa-produto"><img src="'+produto.imagem+'" alt=""><h1>'+produto.produto+'</h1><div id="caixa-produto2"><h2 id="preco">'+produto.preco+'</h2><button>Comprar</button></div></div>';
+});
+
+document.getElementById('container').innerHTML = conteudoProdutos;
