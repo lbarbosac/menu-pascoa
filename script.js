faf3ce6133a3506bcc3bfd52e88eb1f055fe6868
Lucas Barbosa <lucas2304barbosa@gmail.com>
2025-04-22 11:24:12 -0300

    arrumar carrinho

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 const produtos = [
-    {imagem:'https://www.americanas.com.br/_next/image?url=https%3A%2F%2Famericanas.vtexassets.com%2Farquivos%2Fids%2F462397%2F1638790540_1SZ.jpg%3Fv%3D638796563477300000&w=1280&q=75', produto: 'Ovo de Páscoa Sonho de Valsa', preco: 59.90},
-    {imagem: 'https://www.americanas.com.br/_next/image?url=https%3A%2F%2Famericanas.vtexassets.com%2Farquivos%2Fids%2F577302%2F7514250775_1SZ.jpg%3Fv%3D638796546541430000&w=1280&q=75', produto: 'Ovo de Páscoa Chocolate ao Leite', preco: 69.90},
-    {imagem: 'https://www.americanas.com.br/_next/image?url=https%3A%2F%2Famericanas.vtexassets.com%2Farquivos%2Fids%2F408669%2F7087418610_1SZ.jpg%3Fv%3D638790693546970000&w=1280&q=75', produto: 'Ovo de Páscoa Patrulha Canina', preco: 79.90},
-    {imagem: 'https://www.americanas.com.br/_next/image?url=https%3A%2F%2Famericanas.vtexassets.com%2Farquivos%2Fids%2F30046134%2F2893971989_1SZ.jpg%3Fv%3D638793881612100000&w=1280&q=75', produto: 'Ovo de Páscoa Ouro Branco 359g', preco: 89.90},
-    {imagem: 'https://www.americanas.com.br/_next/image?url=https%3A%2F%2Famericanas.vtexassets.com%2Farquivos%2Fids%2F31256941%2F7622201676049_1%20-1-.jpg.jpg%3Fv%3D638798015736670000&w=1280&q=75', produto: 'Ovo de Páscoa Triplo Avelã 320g', preco: 99.99}
+    {imagem:'testes-js/img/ovo-sonho-de-valsa-sem-fundo.png', produto: 'Ovo de Páscoa Sonho de Valsa', preco: 59.90},
+    {imagem: 'testes-js/img/ovo-bis-chocolate-sem-fundo.png', produto: 'Ovo de Páscoa Chocolate ao Leite', preco: 69.90},
+    {imagem: 'testes-js/img/ovo-patrulha-canina-sem-fundo.png', produto: 'Ovo de Páscoa Patrulha Canina', preco: 79.90},
+    {imagem: 'testes-js/img/ovo-ouro-branco-sem-fundo.png', produto: 'Ovo de Páscoa Ouro Branco 359g', preco: 89.90},
+    {imagem: 'testes-js/img/ovo-tripla-camada-avela-sem-fundo.png', produto: 'Ovo de Páscoa Triplo Avelã 320g', preco: 99.99}
 ];
 
 let conteudoProdutos = '';
@@ -42,7 +42,18 @@ diminuirButton.addEventListener('click',() => {
 
 function abrirCarrinho() {
     const carrinho = document.getElementById("container-carrinho");
-    
+    carrinho.style.display = 'flex';
+};
+
+function fecharCarrinho(){
+    const carrinho = document.getElementById("container-carrinho");
+    carrinho.style.display = 'none';
 }
+
+//const btnAbrir = document.getElementById('btnAbrir');
+//btnAbrir.addEventListener('click', abrirCarrinho);
+
+//const btnFechar = document.getElementById('btnFechar');
+//btnFechar.addEventListener('click', fecharCarrinho)
 
 
