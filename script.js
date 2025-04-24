bf78261e152d9f2b3242d3400fc1f6f5ba6dc203
Lucas Barbosa <lucas2304barbosa@gmail.com>
2025-04-24 12:24:48 -0300

    .

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ const produtos = [
 ];
 
 let conteudoProdutos = '';
+let contador = 0;
 produtos.forEach(function(produto){
-    conteudoProdutos += '<div class="caixa-produto"><img src="'+produto.imagem+'" alt=""><h1>'+produto.produto+'</h1><div id="caixa-produto2"><h2 id="preco"> R$ '+produto.preco+'</h2><button>Comprar</button></div></div>';
+    conteudoProdutos += '<div class="caixa-produto"><img src="'+produto.imagem+'" alt=""><h1>'+produto.produto+'</h1><div id="caixa-produto2"><h2 id="preco"> R$ '+produto.preco+'</h2><button data-value="'+contador+'">Comprar</button></div></div>';
+    contador++;
 });
 
 document.getElementById('container').innerHTML = conteudoProdutos;
@@ -59,24 +61,30 @@ function fecharCarrinho() {
 // aqui vai fechar a caixa de compra que esta lá dentro do carrinho, caixa de produtos
 
 function fecharCaixaDeCompra() {
-    const fecharProduto = document.querySelector('.caixa-compra');
+    const fecharProduto = document.querySelector('button[data-valeu]');
     fecharProduto.style.display = 'none';
 };
 
 //  caixa de produtos
 
-const buttonComprar = document.getElementById('comprar');
-comprar.addEventListener('click',() => {
-    const produtosSelecionados = [
-        {img: 'img/ovo-sonho-de-valsa-sem-fundo.png', nomeProduto: 'Ovo de Páscoa Sonho de Valsa', precoProduto: 59.90},
-        {img: 'img/ovo-bis-chocolate-sem-fundo.png', nomeProduto: 'Ovo de Páscoa Chocolate ao Leite', precoProduto: 69.90},
-        {img: 'img/ovo-patrulha-canina-sem-fundo.png', nomeProduto: 'Ovo de Páscoa Patrulha Canina', precoProduto: 79.90},
-        {img: 'img/ovo-ouro-branco-sem-fundo.png', nomeProduto: 'Ovo de Páscoa Ouro Branco 359g', precoProduto: 89.90},
-        {img: 'img/ovo-tripla-camada-avela-sem-fundo.png', nomeProduto: 'Ovo de Páscoa Triplo Avelã 320g', precoProduto: 99.99}
-    ];
-    
-    let conteudoCaixaProdutosSelecionados = '';
-    produtosSelecionados.forEach(function(selecionados){
-        conteudoCaixaProdutosSelecionados += '<div id="main-carrinho"><div class="caixa-compra"><img src="'+produtosSelecionados.img+'" alt=""><h1>'+produtosSelecionados.nomeProduto+'</h1><button onclick="fecharCaixaDeCompra()" id="remover">X</button><div class="info-linha"><div id="contador"><button class="botoes-contador" id="diminuir">-</button><input type="text" value="0" disabled id="quantidade"><button class="botoes-contador" id="somar">+</button></div><div id="soma-preco">R$, '+produtosSelecionados.precoProduto+'</div></div></div></div>'
+const buttonComprar = document.querySelectorAll('button[data-value]');
+buttonComprar.forEach(button => {
+    button.addEventListener('click', function () {
+        const index = this.getAttribute('data-value');
+        const produtoSelecionado = produtos[index];
+        const carrinho = document.getElementById("main-carrinho")
+        carrinho.innerHTML = `<div class="caixa-compra">
+                <img src="${produtos[index].imagem}" alt="${produtos[index].produto}">
+                <h1>${produtos[index].produto}</h1>
+                <button onclick="fecharCaixaDeCompra()" class="remover">X</button>
+                <div class="info-linha">
+                    <div class="contador">
+                        <button class="botoes-contador" class="diminuir" id="diminuir">-</button>
+                        <input type="text" value="0" disabled class="quantidade" id="quantidade">
+                        <button class="botoes-contador" class="somar" id="somar" style="color: #28a745;">+</button>
+                    </div>]
+                    <div class="soma-preco">${produtos[index].preco}</div>
+                </div>
+            </div>`
     });
 });
